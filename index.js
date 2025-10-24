21877c629d403477c00f11edc859455f409237f4
Kashika Ghosh <kashika0613@gmail.com>
2025-10-24 19:07:32 +0530

    index route

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const mongoose = require("mongoose");
 const mongo_url = "mongodb://127.0.0.1:27017/travelxgo";
 const Listing = require("./models/listing.js");
+const path = require("path");
+
 main()
 .then(() => {
  console.log("connected to db");
@@ -14,23 +16,32 @@ async function main() {
     await mongoose.connect(mongo_url);
     
 }
-app.get("/testlisting", async (req, res) => {
-   try {
-      let samplelisting = new Listing({
-         title: "view from my place",
-         description: "mountains",
-         price: 2000,
-         location: "kashmir",
-         country: "India",
-      });
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+
+//app.get("/testlisting", async (req, res) => {
+  // try {
+    //  let samplelisting = new Listing({
+      //   title: "view from my place",
+        // description: "mountains",
+       //  price: 2000,
+      //   location: "kashmir",
+        // country: "India",
+     // });
       
-      await samplelisting.save();
-      console.log("sample was saved:", samplelisting);
-      res.send("successful testing");
-   } catch (error) {
-      console.log("Error saving:", error);
-      res.send("Error occurred");
-   }
+   //   await samplelisting.save();
+   //   console.log("sample was saved:", samplelisting);
+   //   res.send("successful testing");
+  // } catch (error) {
+   //   console.log("Error saving:", error);
+   //   res.send("Error occurred");
+  // }
+//});
+
+app.get("/listings", async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("listings/index.ejs", {allListings});
+
 });
 app.get("/",(req,res) => {
     res.send("hi , I am groot");
