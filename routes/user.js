c5e2afceeab35dfbbeec914125c545857668499e
Kashika Ghosh <kashika0613@gmail.com>
2026-03-15 23:08:51 +0530

    logout: git commit -m logout:

--- routes/user.js
+++ routes/user.js
@@ -30,5 +30,13 @@ router.post("/login", passport.authenticate("local",{failureRedirect:"/login",fa
     res.redirect("/listings");
 
 })
-
+router.get("/logout",async(req,res,next) => {
+    req.logout((err)=> {
+        if(err) {
+            return next(err);
+        }
+        req.flash("success","you are logged out successfully");
+        res.redirect("/listings");
+    })
+})
 module.exports = router;
