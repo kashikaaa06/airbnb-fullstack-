d7a1b201574866a960f4ddc203600ead846022a1
Kashika Ghosh <kashika0613@gmail.com>
2026-03-27 21:55:09 +0530

    changes in navbar

--- index.js
+++ index.js
@@ -61,6 +61,7 @@ passport.deserializeUser(User.deserializeUser());
 app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
+    res.locals.currentuser = req.user;
     next();
 });
 
