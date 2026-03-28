4d306209051c96479a306e60043d1a31dcb38568
Kashika Ghosh <kashika0613@gmail.com>
2026-03-28 22:54:30 +0530

    post login page

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require("../models/user.js"); 
 const wrapAsync = require("../utils/wrapAsync.js");
 const passport = require("passport");
+
 router.get("/signup", async(req,res) => {
     res.render("user/signup.ejs");
 })
@@ -14,8 +15,14 @@ router.post("/signup",wrapAsync( async(req,res)=> {
     try {
         const registeruser = await User.register(user, password);
         console.log(registeruser);
-        req.flash("success", "Welcome to travelxgo");
-        res.redirect("/listings");
+        req.login(registeruser,(err) => {
+            if(err) {
+                return next(err);
+            }
+            req.flash("success","welcome to travelxgo!");
+            res.redirect("/listings");
+        })
+
     } catch(err) {
         req.flash("error", err.message);
         res.redirect("/signup");
